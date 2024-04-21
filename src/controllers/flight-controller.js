8fa5ed0ff51ccd61c9170841b6dd048182c37920
kuldeep Raj Gour <kuldeepgour002@gmail.com>
2024-04-21 16:51:57 +0530

    added get all function

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -7,7 +7,7 @@ const create  = async(req,resp)=>
 {
     try {
         const flight = await flightService.createFlight(req.body);
-        return resp.status(201).json({
+        return resp.status(200).json({
             data: flight,
             success: true,
             err: {},
@@ -28,6 +28,32 @@ const create  = async(req,resp)=>
 
 }
 
+const getAll = async(req,resp)=>{
+    try{
+
+        const response = await flightService.getAllFlightData(req.query);
+        return resp.status(200).json({
+            data: response,
+            success: true,
+            err: {},
+            message: 'Successfully fetched a flight'
+    })
+
+    }
+    catch(error)
+    {
+        console.log(error);
+        return resp.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to fetch the  flight',
+            err: error
+        });
+
+    }
+}
+
 module.exports={
-    create
+    create,
+    getAll
 }
