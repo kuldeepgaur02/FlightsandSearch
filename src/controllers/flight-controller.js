3ff8f79ceb704571b81626e7fede0a2a282c9c11
kuldeep Raj Gour <kuldeepgour002@gmail.com>
2024-04-28 15:53:56 +0530

    added flight req middle ware

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -1,59 +1,101 @@
-const { Model } = require('sequelize');
 const {FlightService} = require('../services/index');
+const { SuccessCodes } = require('../utils/error-codes');
 
 const flightService = new FlightService();
 
-const create  = async(req,resp)=>
-{
+const create = async (req, res) => {
     try {
-        const flight = await flightService.createFlight(req.body);
-        return resp.status(200).json({
+        const flightRequestData = {
+            flightNumber: req.body.flightNumber,
+            airplaneId: req.body.airplaneId,
+            departureAirportId: req.body.departureAirportId,
+            arrivalAirportId: req.body.arrivalAirportId,
+            arrivalTime: req.body.arrivalTime,
+            departureTime: req.body.departureTime,
+            price: req.body.price
+        }
+        const flight = await flightService.createFlight(flightRequestData);
+        return res.status(SuccessCodes.CREATED).json({
             data: flight,
             success: true,
             err: {},
             message: 'Successfully created a flight'
-    })
-
-    }
-    catch(error)
-    {
+        })
+    } catch (error) {
         console.log(error);
-        return resp.status(500).json({
+        return res.status(500).json({
             data: {},
             success: false,
             message: 'Not able to create a flight',
             err: error
         });
     }
-
 }
 
-const getAll = async(req,resp)=>{
-    try{
-
+const getAll = async (req, res) => {
+    try {
+        console.log(req.query);
         const response = await flightService.getAllFlightData(req.query);
-        return resp.status(200).json({
+        return res.status(SuccessCodes.OK).json({
             data: response,
             success: true,
             err: {},
-            message: 'Successfully fetched a flight'
-    })
-
-    }
-    catch(error)
-    {
+            message: 'Successfully fetched the flights'
+        });
+    } catch (error) {
         console.log(error);
-        return resp.status(500).json({
+        return res.status(500).json({
             data: {},
             success: false,
-            message: 'Not able to fetch the  flight',
+            message: 'Not able to fetch the flights',
             err: error
         });
-
     }
 }
 
-module.exports={
+const get = async (req, res) => {
+    try {
+        const response = await flightService.getFlight(req.params.id);
+        return res.status(SuccessCodes.OK).json({
+            data: response,
+            success: true,
+            err: {},
+            message: 'Successfully fetched the flight'
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to fetch the flight',
+            err: error
+        });
+    }
+}
+
+const update = async (req, res) => {
+    try {
+        const response = await flightService.updateFlight(req.params.id, req.body);
+        return res.status(SuccessCodes.OK).json({
+            data: response,
+            success: true,
+            err: {},
+            message: 'Successfully updated the flight'
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to update the flight',
+            err: error
+        });
+    }
+}
+
+module.exports = {
     create,
-    getAll
+    getAll,
+    get,
+    update
 }
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const { FlightMiddlewares } = require('../../middlewares/index');
+
 const CityController = require('../../controllers/city-controllers');
 
 const FlightController = require('../../controllers/flight-controller');
@@ -18,7 +20,11 @@ router.get('/city',CityController.getAll);
 
 router.patch('/city/:id',CityController.update);
 
-router.post('/flights',FlightController.create);
+router.post(
+    '/flights', 
+    FlightMiddlewares.validateCreateFlight, 
+    FlightController.create
+);
 
 router.get('/flights',FlightController.getAll);
 
